e001bdc8d213b581bf9bc1e59c6a3264b3b6ac04
Dave Glover <dglover@microsoft.com>
2021-09-05 16:48:10 +1000

    Update build-all.ps1

--- tools/build-tools/build-all.ps1
+++ tools/build-tools/build-all.ps1
@@ -2,7 +2,7 @@
 
 $gnupath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Linux\gcc_arm\bin"
 
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -path ./build
+$exit_code = 0
 
 function build-high-level {
     param (
@@ -43,49 +43,62 @@ function build-real-time {
     cmake --build .
 }
 
-$dirlist = Get-ChildItem -Recurse -Directory -Name -Depth 2
-foreach ($dir in $dirList) {
 
-    $manifest = Join-Path -Path $dir -ChildPath "app_manifest.json"
-    $cmakefile = Join-Path -Path $dir -ChildPath "CMakeLists.txt"    
+function find_projects {
 
-    if ((Test-Path -path $manifest) -and (Test-Path -path $cmakefile) ) {
+    $dirlist = Get-ChildItem -Recurse -Directory -Name -Depth 2
+    foreach ($dir in $dirList) {
 
-        # Only build executable apps not libraries
-        if (Select-String -Path $cmakefile -Pattern 'add_executable' -Quiet ) {
+        $manifest = Join-Path -Path $dir -ChildPath "app_manifest.json"
+        $cmakefile = Join-Path -Path $dir -ChildPath "CMakeLists.txt"    
 
-            # Is this a high-level app?
-            if (Select-String -Path $manifest -Pattern 'Default' -Quiet) {
+        if ((Test-Path -path $manifest) -and (Test-Path -path $cmakefile) ) {
 
-                build-high-level($dir)
-            }
-            else {
-                # Is this a real-time app?
-                if (Select-String -Path $manifest -Pattern 'RealTimeCapable' -Quiet) {
+            # Only build executable apps not libraries
+            if (Select-String -Path $cmakefile -Pattern 'add_executable' -Quiet ) {
 
-                    build-real-time($dir)
+                # Is this a high-level app?
+                if (Select-String -Path $manifest -Pattern 'Default' -Quiet) {
+
+                    build-high-level($dir)
                 }
                 else {
-                    # Not a high-level or real-time app so just continue
-                    continue
+                    # Is this a real-time app?
+                    if (Select-String -Path $manifest -Pattern 'RealTimeCapable' -Quiet) {
+
+                        build-real-time($dir)
+                    }
+                    else {
+                        # Not a high-level or real-time app so just continue
+                        continue
+                    }
+                } 
+
+                # was the imagepackage created - this is a proxy for sucessful build
+                $imagefile = Get-ChildItem "*.imagepackage" -Recurse
+                if ($imagefile.count -ne 0) {
+                    Write-Output "`nSuccessful build: $dir.`n"
                 }
-            } 
+                else {
+                    Set-Location ".."
+                    Write-Output "`nBuild failed: $dir.`n"
 
-            # was the imagepackage created - this is a proxy for sucessful build
-            $imagefile = Get-ChildItem "*.imagepackage" -Recurse
-            if ($imagefile.count -ne 0) {
-                Write-Output "`nSuccessful build: $dir.`n"
-            }
-            else {
+                    $script:exit_code = 1
+                    
+                    break
+                }
+
                 Set-Location ".."
-                Write-Output "`nBuild failed: $dir.`n"
-                break
+                Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -path ./build
             }
-
-            Set-Location ".."
-            Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -path ./build
         }
     }
 }
 
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -path ./build
+
+find_projects
+
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -path ./build
+
+exit $exit_code
