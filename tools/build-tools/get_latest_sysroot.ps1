85454b36aa98ba74fd3894af0758ccecf9e506e8
Dave Glover <dglover@microsoft.com>
2021-09-06 12:15:30 +1000

    Updates for update_config and build_all scripts

--- tools/build-tools/get_latest_sysroot.ps1
+++
@@ -1,4 +0,0 @@
-
-$sysroots = get-childitem "C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots"  | Sort-Object -Descending -Property {$_.Name -as [int]}
-
-Write-Output  $sysroots[0].Name
--- tools/build-tools/get_tools_version.ps1
+++
@@ -1,9 +0,0 @@
-$tools_version = "?.?"
-
-$azure_sphere_sdk_version = azsphere show-version -o tsv
-$version_parts = $azure_sphere_sdk_version.split('.')
-if ($version_parts.count -gt 2) {
-    $local:tools_version=  $version_parts[0] + '.' + $version_parts[1]
-}
-
-Write-Output $tools_version
