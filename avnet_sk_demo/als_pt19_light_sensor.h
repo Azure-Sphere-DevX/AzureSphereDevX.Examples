acbf5c02ab5890cbc9dcb2ace5673e0c12662f21
Brian W <brian@willess.com>
2021-09-20 10:06:31 -0400

    Add packed attribute to structs

--- avnet_sk_demo/als_pt19_light_sensor.h
+++ avnet_sk_demo/als_pt19_light_sensor.h
@@ -5,8 +5,7 @@
 
 // Define the different messages IDs we can send to real time applications
 // If this enum is changed, it also needs to be changed for the high level application
-typedef enum
-{
+typedef enum __attribute__((packed)) {
 	IC_UNKNOWN, 
     IC_HEARTBEAT,
 	IC_READ_SENSOR, 
@@ -15,8 +14,7 @@ typedef enum
 } INTER_CORE_CMD;
 
 // Define the expected data structure.  Note this struct came from the AvnetGroveGPS real time application code
-typedef struct
-{
+typedef struct __attribute__((packed)) {
     INTER_CORE_CMD cmd;
     uint32_t sensorSampleRate;
     uint32_t sensorData;
