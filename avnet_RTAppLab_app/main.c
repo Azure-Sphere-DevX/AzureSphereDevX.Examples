b634df8967c474bfed8ccb583e1023f38f52f2a8
Brian W <brian@willess.com>
2022-02-03 07:56:08 -0700

    Working app to drive RTApp from Lab

--- avnet_RTAppLab_app/main.c
+++ avnet_RTAppLab_app/main.c
@@ -41,30 +41,125 @@
  ************************************************************************************************/
 #include "main.h"
 
+static DX_DECLARE_TIMER_HANDLER(IntercoreSendLedUpdateHandler);
+static DX_DECLARE_TIMER_HANDLER(IntercoreSendStringUpdateHandler);
+
 /****************************************************************************************
  * Implementation
  ****************************************************************************************/
-// TODO: Add all your handlers and helper functions here
+/// <summary>
+//// Toggle the LED state and send the new state to the real-time app to turn the LED on or off
+/// </summary>
+static DX_TIMER_HANDLER(IntercoreSendLedUpdateHandler)
+{
+    static bool ledState = true;
+
+    Log_Debug("New LED State: %s\n", ledState ? "true": "false");
+    ledState = !ledState; 
+
+    // reset inter-core block
+    memset(&ic_tx_block, 0x00, sizeof(IC_COMMAND_BLOCK_SAMPLE_HL_TO_RT));
+
+    // Set the command 
+    ic_tx_block.cmd = IC_SAMPLE_CONTROL_LED;
+    
+    // Copy the new ledState into the ic_block
+    ic_tx_block.ledOn = ledState;
+
+    // Send the message to the real-time application
+    dx_intercorePublish(&intercore_app_asynchronous, &ic_tx_block,
+                            sizeof(IC_COMMAND_BLOCK_SAMPLE_HL_TO_RT));
+
+}
+DX_TIMER_HANDLER_END
+
+/// <summary>
+/// Construct a string and send it to the real-time app.  We expect the real-time app to 
+/// transfer the string over a UART (looped back to itself), reverse the string and send
+/// the reversed string back to this high-level application.
+/// </summary>
+static DX_TIMER_HANDLER(IntercoreSendStringUpdateHandler)
+{
+
+    // Setup a char array to hold the string we send to the real-time app
+    static char dynamicString[128+1] = {0};
+    static int currentStringLenth = 1;
+
+    char currentChar = ' '; // 0x20) < 0x5f)
+
+    for(int i = 0; i < currentStringLenth; i++){
+        dynamicString[i] = currentChar++;
+
+        if(currentChar >= '^'){
+            currentChar = ' ';
+        }
+    }
+
+    dynamicString[currentStringLenth+1] = '\0';
+    Log_Debug("TX: \"%s\"\n", dynamicString);
+
+    if(++currentStringLenth > 128){
+        currentStringLenth = 1;
+    }
+
+    // reset inter-core block
+    memset(&ic_tx_block, 0x00, sizeof(IC_COMMAND_BLOCK_SAMPLE_HL_TO_RT));
+
+    // Set the command 
+    ic_tx_block.cmd = IC_SAMPLE_TX_STRING;
+    
+    // Copy the dynamic string into the ic_block
+    strncpy(ic_tx_block.stringData, dynamicString, strnlen(dynamicString, 128));
+    
+    // Send the message to the real-time application
+    dx_intercorePublish(&intercore_app_asynchronous, &ic_tx_block,
+                            sizeof(IC_COMMAND_BLOCK_SAMPLE_HL_TO_RT));
+
+}
+DX_TIMER_HANDLER_END
+
+/// <summary>
+/// Callback handler for Asynchronous Inter-Core Messaging Pattern
+/// </summary>
+static void IntercoreResponseHandler(void *data_block, ssize_t message_length)
+{
+    IC_COMMAND_BLOCK_LAB_CMDS_RT_TO_HL *ic_rx_block = (IC_COMMAND_BLOCK_LAB_CMDS_RT_TO_HL *)data_block;
+
+    switch (ic_rx_block->cmd) {
+    case IC_SAMPLE_UNKNOWN:
+    case IC_SAMPLE_HEARTBEAT:
+    case IC_SAMPLE_READ_SENSOR_RESPOND_WITH_TELEMETRY:
+	case IC_SAMPLE_SET_AUTO_TELEMETRY_RATE:
+    case IC_SAMPLE_READ_SENSOR:
+    case IC_SAMPLE_CONTROL_LED:
+        // Handle these cases, but don't do anything
+        break;
+    case IC_SAMPLE_TX_STRING:
+        Log_Debug("RX: \"%s\"\n", ic_rx_block->stringData);
+        break;
+
+    default:
+        break;
+    }
+}
 
 /// <summary>
 ///  Initialize peripherals, device twins, direct methods, timer_bindings.
 /// </summary>
 static void InitPeripheralsAndHandlers(void)
-{
-#ifdef USE_AVNET_IOTCONNECT
-    dx_avnetConnect(&dx_config, NETWORK_INTERFACE);
-#else     
-//    dx_azureConnect(&dx_config, NETWORK_INTERFACE, IOT_PLUG_AND_PLAY_MODEL_ID);
-#endif     
-    
+{    
+    // Open the connection to the real-time application
+    if(!dx_intercoreConnect(&intercore_app_asynchronous)){
+        Log_Debug("Inter-core connection failed . . .\n");
+        Log_Debug(". . . Verify RTApp is running before starting this high-level application\n");
+        Log_Debug(". . . Verify the RTApp component ID matches the Component ID in the inter-core binding in this application\n");
+        dx_terminate(APP_ExitCode_InterCore_Connect_Error);
+    }
+
     dx_gpioSetOpen(gpio_bindings, NELEMS(gpio_bindings));
     dx_timerSetStart(timer_bindings, NELEMS(timer_bindings));
     dx_deviceTwinSubscribe(device_twin_bindings, NELEMS(device_twin_bindings));
     dx_directMethodSubscribe(direct_method_bindings, NELEMS(direct_method_bindings));
-
-    // TODO: Update this call with a function pointer to a handler that will receive connection status updates
-    // see the azure_end_to_end example for an example
-    // dx_azureRegisterConnectionChangedNotification(NetworkConnectionState);
 }
 
 /// <summary>
@@ -82,10 +177,6 @@ static void ClosePeripheralsAndHandlers(void)
 int main(int argc, char *argv[])
 {
     dx_registerTerminationHandler();
-
-    if (!dx_configParseCmdLineArguments(argc, argv, &dx_config)) {
-        return dx_getTerminationExitCode();
-    }
 
     InitPeripheralsAndHandlers();
 
--- avnet_RTAppLab_app/main.h
+++ avnet_RTAppLab_app/main.h
@@ -11,64 +11,35 @@
 #include "dx_version.h"
 #include <applibs/log.h>
 #include <applibs/applications.h>
-
-// Use main.h to define all your application definitions, message properties/contentProperties,
-// bindings and binding sets.
-
-// https://docs.microsoft.com/en-us/azure/iot-pnp/overview-iot-plug-and-play
-#define IOT_PLUG_AND_PLAY_MODEL_ID "" //TODO insert your PnP model ID if your application supports PnP
-
-// Details on how to connect your application using an ethernet adaptor
-// https://docs.microsoft.com/en-us/azure-sphere/network/connect-ethernet
-#define NETWORK_INTERFACE "wlan0"
-
-#define SAMPLE_VERSION_NUMBER "1.0"
-#define ONE_MS 1000000
-
-DX_USER_CONFIG dx_config;
-
-/****************************************************************************************
- * Avnet IoTConnect Support
- ****************************************************************************************/
-// TODO: If the application will connect to Avnet's IoTConnect platform enable the 
-// #define below
-//#define USE_AVNET_IOTCONNECT
-
-/****************************************************************************************
- * Application defines
- ****************************************************************************************/
-// TODO: Add any application constants
+#include "generic_rt_app.h"
+#include "dx_intercore.h"
 
 /****************************************************************************************
  * Forward declarations
  ****************************************************************************************/
-// TODO: Add all Forward declarations here or in main.c
+static void IntercoreResponseHandler(void *data_block, ssize_t message_length);
+static DX_DECLARE_TIMER_HANDLER(IntercoreSendLedUpdateHandler);
+static DX_DECLARE_TIMER_HANDLER(IntercoreSendStringUpdateHandler);
 
 /****************************************************************************************
- * Telemetry message buffer property sets
+ * InterCore TX/RX block structures
  ****************************************************************************************/
-
-// Number of bytes to allocate for the JSON telemetry message for IoT Hub/Central
-// TODO: Remove comments to use the global message buffer for sending telemetry
-//#define JSON_MESSAGE_BYTES 256
-//static char msgBuffer[JSON_MESSAGE_BYTES] = {0};
-
-// TODO: Define telemetry message properties here, for example . . . 
-//static DX_MESSAGE_PROPERTY *messageProperties[] = {&(DX_MESSAGE_PROPERTY){.key = "appid", .value = "hvac"}, 
-//                                                   &(DX_MESSAGE_PROPERTY){.key = "type", .value = "telemetry"},
-//                                                   &(DX_MESSAGE_PROPERTY){.key = "schema", .value = "1"}};
-
-// TODO: Remove comments to define contentProperties for sending telemetry
-//static DX_MESSAGE_CONTENT_PROPERTIES contentProperties = {.contentEncoding = "utf-8", .contentType = "application/json"};
+IC_COMMAND_BLOCK_LAB_CMDS_RT_TO_HL ic_rx_block;
+IC_COMMAND_BLOCK_SAMPLE_HL_TO_RT ic_tx_block;
 
 /****************************************************************************************
  * Bindings
  ****************************************************************************************/
-// TODO: Declare all bindings here, for example . . . 
+DX_INTERCORE_BINDING intercore_app_asynchronous = {.nonblocking_io = true,
+                                                   .rtAppComponentId = "f6768b9a-e086-4f5a-8219-5ffe9684b001",
+                                                   .interCoreCallback = IntercoreResponseHandler,
+                                                   .intercore_recv_block = &ic_rx_block,
+                                                   .intercore_recv_block_length = sizeof(ic_rx_block)};
 
-//static DX_DEVICE_TWIN_BINDING dt_desired_sample_rate = {.propertyName = "DesiredSampleRate", .twinType = DX_DEVICE_TWIN_INT, .handler = dt_desired_sample_rate_handler};
-//static DX_GPIO_BINDING gpio_led = {.pin = LED2, .name = "gpio_led", .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true};
-//static DX_TIMER_BINDING tmr_publish_message = {.period = {4, 0}, .name = "tmr_publish_message", .handler = publish_message_handler};
+// Timers
+static DX_TIMER_BINDING tmr_IntercoreSendLedUpdate = {.period = {1, 0}, .name = "IntercoreSendLedUpdateHandler", .handler = IntercoreSendLedUpdateHandler};
+static DX_TIMER_BINDING tmr_IntercoreSendStringUpdate = {.period = {1, 0}, .name = "IntercoreSendStringUpdateHandler", .handler = IntercoreSendStringUpdateHandler};
+
 
 /****************************************************************************************
  * Binding sets
@@ -79,4 +50,4 @@ DX_USER_CONFIG dx_config;
 DX_DEVICE_TWIN_BINDING *device_twin_bindings[] = {};
 DX_DIRECT_METHOD_BINDING *direct_method_bindings[] = {};
 DX_GPIO_BINDING *gpio_bindings[] = {};
-DX_TIMER_BINDING *timer_bindings[] = {};
+DX_TIMER_BINDING *timer_bindings[] = {&tmr_IntercoreSendLedUpdate, &tmr_IntercoreSendStringUpdate};
